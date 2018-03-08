34ef0d4cab9252d7dd696a720cdb23dd14d7fbc2
SeoboS <Seobo11shim@gmail.com>
2018-03-08 02:52:33 -0500

    cleaned up UI and added download buttons for the results

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # RUNetIDChecker
 Automates checking a RU student's netid against a search query
+
+Features:
+* Reads a .csv file with a list of netids and search queries.
+
+
+
+.csv file format example (can have unlimited amount of search terms):
+
+netid,searchterm1,searchterm2
+ss2468,SCHOOL OF ENGINEERING
+al93,SCHOOL OF ENGINEERING,/18
+yu1,SCHOOL OF ARTS,/17
+
+output:
+
+true
+true, false
+false, true
+
+searchTerm1 found, searchTerm2 found
+al93 is in school of engineering and is not graduating in 2018
+yu1 is not in school of arts and science and is graduating in 2017
--- main.js
+++ main.js
@@ -6,23 +6,22 @@ var errors = [];
 var results = [];
 var data;
 var dataCnt = 0;
+var resultFileName = "results.txt";
+var errorFileName = "errors.txt";
 
 function main(){
 	var netid;
 	var searchTerm;
-
-	//var netids = ["ss2468","jtm209","sd912"];
-	//var searchTerms = ["SCHOOL OF ENGINEERING","SCHOOL OF ENGINEERING","SCHOOL OF ENGINEERING"];
 	document.getElementById('fileInput').addEventListener('change',readFile);
-	//$("#batchUserLookup").click(main);
 }
 
 function readFile(e){
-	//var req = new FileReader();
+	//only reads xls, csv, or txt files
+	console.log("readingfile");
 	var file = fileInput.files[0];
-	var textType = /text.*/;
+	var excel = "application/vnd.ms-excel";
 
-	if (file.type.match(textType)){
+	if (file.type.match(excel) || file.type.match(/text.*/)){
 		var reader = new FileReader();
 		reader.onload = function(){
 			data = $.csv.toArrays(reader.result);
@@ -32,27 +31,25 @@ function readFile(e){
 
 		reader.readAsText(file);
 	} 
+	else{
+		throw "Not valid. File type: " + file.type;
+	}
 }
 
 
 function searchMain(data){
 
 	dataCnt = dataCnt + 1;
-	console.log("main running");
 	netid = data[dataCnt][0];
-	//netid = netids.pop();
-	console.log(netid);
 	if (data[dataCnt].length == 2){
 		searchTerm = data[dataCnt][1];
 	}
-	else{
+	else{// TODO make multiple search terms
 		searchTerm = [];
 		for (var i = data[dataCnt].length-1; i > 0; --i){
 			searchTerm.push(data[dataCnt][i]);
 		}
 	}
-	//searchTerm = searchTerms.pop();
-	//searchTerm = inputs[1]; // TODO make multiple search terms
 
 	chrome.runtime.onMessage.addListener(lookUpAnotherUser);
 	chrome.webNavigation.onCommitted.addListener( function(details){
@@ -60,7 +57,7 @@ function searchMain(data){
 			chrome.tabs.executeScript(null,{file: "currentPage.js"},checkPageAndSearch);
 		}
 		else{
-			console.log("auto subframes here");
+			console.log("Auto subframes loading detected");
 		}
 	});
 
@@ -68,9 +65,9 @@ function searchMain(data){
 }
 
 function checkPageAndSearch(page){
-	console.log(searchTerm);
+	//console.log(searchTerm);
 	page=page[0];
-	console.log(page);
+	//console.log(page);
 	if (page=="Info"){ // TODO adjust to handle multiple search terms
 		chrome.tabs.executeScript(null, {code: 'var searchTerm = ' + JSON.stringify(searchTerm)}, function(){
 			chrome.tabs.executeScript(null,{file: "checkPage.js"}, function(){
@@ -98,10 +95,8 @@ function lookUpAnotherUser(result){
 }
 
 function storeResult(result){
-	console.log(results);
 	results.push(result); // allow for multiple results
 	//results.push(result);
-	console.log(result);
 	if (data.length == dataCnt+1){ // if all netids checked
 		res = results.reverse();
 		err = errors.reverse();
@@ -110,13 +105,14 @@ function storeResult(result){
 		console.log("results:");
 		console.log(res);
 		console.log("EXIT");
+		downloadResult(resultFileName,res);
+		downloadResult(errorFileName,err);
 		return 0;
 	}
 	else{ // else update inputs
 		++dataCnt;
 		//netid = netids.pop();
 		netid = data[dataCnt][0];
-		console.log(netid);
 		//searchTerm = searchTerms.pop();
 		if (data[dataCnt].length == 2){
 			searchTerm = data[dataCnt][1];
@@ -131,8 +127,17 @@ function storeResult(result){
 	}
 }
 
+function downloadResult(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.setAttribute('id',filename);
+
+  element.appendChild(document.createTextNode(filename));
+  document.body.appendChild(element);
+}
+
 function submitForm(netid){
-	console.log("submitting form");
 	chrome.tabs.executeScript(null, {code: 'var netid = ' + JSON.stringify(netid)}, function(){
 		if (chrome.runtime.lastError) {
 		  console.log('There was an error injecting script : \n' + chrome.runtime.lastError.message);
@@ -148,7 +153,7 @@ function submitForm(netid){
 
 function sendMessageToPageScript(message){
 	if (message == null){
-		console.log("message sent is null");
+		throw "Message sent is null";
 	}
 	// sends the search term to get it's page checked
 	chrome.tabs.query({active:true, currentWindow: true}, function(tab){
